5cc693b55fd09a666faae098a74038fea098a5a4
Tq-Lj <6du@protonmail.com>
2019-08-03 21:20:40 +0800

    init

---
+++ cloudflare-6du.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+require("livescript-transform-implicit-async/register");
+require('./cloudflare.ls');
+
---
+++ cloudflare-6du.ls
@@ -0,0 +1,6 @@
+require! <[
+  path
+]>
+
+do !~>
+  console.log "yes"
