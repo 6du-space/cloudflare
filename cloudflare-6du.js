959b2fbd45e3bb5759370325ca268c75a6fa3d2c
- <->
2019-08-06 14:27:47 +0800

    master

--- cloudflare-6du.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-require("livescript-transform-implicit-async/register");
-require('./cloudflare-6du.ls');
-
--- cloudflare-6du.ls
+++ cloudflare-6du.ls
@@ -1,3 +1,5 @@
+#!/usr/bin/env -S node -r livescript-transform-implicit-async/register
+
 require! <[
   path
   semver
